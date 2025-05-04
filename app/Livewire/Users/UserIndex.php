8634e2183401fc77af41859ae2914fa460f74f29
WaLid-Jbir <anawalid737@gmail.com>
2025-05-04 15:45:27 +0100

    Display all the users in database in a table

--- app/Livewire/Users/UserIndex.php
+++ app/Livewire/Users/UserIndex.php
@@ -3,11 +3,13 @@
 namespace App\Livewire\Users;
 
 use Livewire\Component;
+use App\Models\User;
 
 class UserIndex extends Component
 {
     public function render()
     {
-        return view('livewire.users.user-index');
+        $users = User::all();
+        return view('livewire.users.user-index', compact('users'));
     }
 }
