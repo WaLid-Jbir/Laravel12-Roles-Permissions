bbbb9b87aa0e29d41eea1b24a0638ce4c4e4e674
WaLid-Jbir <anawalid737@gmail.com>
2025-05-26 12:51:24 +0100

    Edit product form

--- app/Livewire/Products/ProductEdit.php
+++ app/Livewire/Products/ProductEdit.php
@@ -3,9 +3,34 @@
 namespace App\Livewire\Products;
 
 use Livewire\Component;
+use App\Models\Product;
 
 class ProductEdit extends Component
 {
+    public $product, $name, $detail;
+
+    public function mount($id)
+    {
+        $this->product = Product::find($id);
+        $this->name = $this->product->name;
+        $this->detail = $this->product->detail;
+    }
+
+    public function submit()
+    {
+        $this->validate([
+            'name' => 'required',
+            'detail' => 'required'
+        ]);
+
+        $this->product->name = $this->name;
+        $this->product->detail = $this->detail;
+
+        $this->product->save();
+
+        return to_route('products.index')->with("success", "Product updated successfully.");
+    }
+
     public function render()
     {
         return view('livewire.products.product-edit');
--- resources/views/livewire/products/product-edit.blade.php
+++ resources/views/livewire/products/product-edit.blade.php
@@ -1,3 +1,24 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <div class="relative mb-6 w-full">
+        <flux:heading size="xl" level="1">{{ __('Edit Product') }}</flux:heading>
+        <flux:subheading size="lg" class="mb-6">{{ __('Edit product details') }}</flux:subheading>
+        <flux:separator variant="subtle" />
+        
+        <!-- Create button aligned to the right -->
+        <div class="flex justify-start mt-4">
+            <a href="{{ route('products.index') }}" class="cursor-pointer rounded-md px-3 py-2 text-xs font-medium text-white bg-blue-500 hover:bg-blue-600">
+                Back
+            </a>
+        </div>
+    </div>
+
+    <div>
+        <form wire:submit="submit">
+            <div class="flex flex-col gap-4">
+                <flux:input wire:model="name" label="Name" placeholder="product name" />
+                <flux:textarea wire:model="detail" label="Detail" placeholder="product detail" />
+            </div>
+            <flux:button type="submit" class="mt-4 cursor-pointer" variant="primary">Submit</flux:button>
+        </form>
+    </div>
 </div>
